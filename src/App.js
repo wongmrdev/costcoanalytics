06c8ccaa1659e8d9e29b567c9167b833cfb83b6c
Matthew Wong <matthew.raymond.wong@gmail.com>
2021-12-05 08:32:00 -1000

    updated auth config for dev/pro

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import './App.css';
 import Amplify, { API, Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
+import awsConfig from './aws-exports';
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import * as queries from './graphql/queries';
 import React, { useState, useEffect, } from 'react';
 import Item from './components/Item';
 import styled from "styled-components";
 import Canvas from "./components/Canvas.js";
-Amplify.configure(awsconfig);
+
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === "[::1]" ||
+  // 127.0.0.1/8 is considered localhost for IPv4.
+  window.location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+  )
+);
+
+// Assuming you have two redirect URIs, and the first is for localhost and second is for production
+const [
+  localRedirectSignIn,
+  productionRedirectSignIn,
+] = awsConfig.oauth.redirectSignIn.split(",");
+
+const [
+  localRedirectSignOut,
+  productionRedirectSignOut,
+] = awsConfig.oauth.redirectSignOut.split(",");
+
+const updatedAwsConfig = {
+  ...awsConfig,
+  oauth: {
+    ...awsConfig.oauth,
+    redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
+    redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
+  }
+}
+
+Amplify.configure(updatedAwsConfig);
 
 export const CouponContext = React.createContext() //allow global access of variables and functions, 
 //must add Context Wrapper Provider with prop of the objects or functions to make global 
