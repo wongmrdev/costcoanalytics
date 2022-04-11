0e5266c55929cabcb1434f481927c4c9c36090bf
mattwongasimov <98349133+mattwongasimov@users.noreply.github.com>
2022-04-11 02:00:06 -0400

    update amplify

--- src/models/index.d.ts
+++ src/models/index.d.ts
@@ -6,17 +6,17 @@ import { ModelInit, MutableModel, PersistentModelConstructor } from "@aws-amplif
 
 export declare class coupon {
   readonly id: string;
-  readonly dateValid?: string;
-  readonly itemNumber?: string;
-  readonly itemName?: string;
-  readonly itemDescription?: string;
-  readonly itemOther?: string;
-  readonly itemVaries?: string;
-  readonly itemShipping?: string;
-  readonly itemYourCost?: string;
-  readonly itemDiscountDollar?: string;
-  readonly itemDiscountCents?: string;
-  readonly itemDiscountLimit?: string;
+  readonly dateValid?: string | null;
+  readonly itemNumber?: string | null;
+  readonly itemName?: string | null;
+  readonly itemDescription?: string | null;
+  readonly itemOther?: string | null;
+  readonly itemVaries?: string | null;
+  readonly itemShipping?: string | null;
+  readonly itemYourCost?: string | null;
+  readonly itemDiscountDollar?: string | null;
+  readonly itemDiscountCents?: string | null;
+  readonly itemDiscountLimit?: string | null;
   constructor(init: ModelInit<coupon>);
   static copyOf(source: coupon, mutator: (draft: MutableModel<coupon>) => MutableModel<coupon> | void): coupon;
 }
