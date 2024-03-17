4622a467e977adc1a511842f797e40213ddaf7ba
mattwongasimov <98349133+mattwongasimov@users.noreply.github.com>
2024-03-17 00:31:41 -0400

    Delete unused files and update coupon validity date

--- costcoCrawlerPutItemsAwsAmplify.js
+++ costcoCrawlerPutItemsAwsAmplify.js
@@ -53,7 +53,7 @@ async function getCoupons() {
       )?.textContent;
       var couponsList = [];
       var couponsList = ecoCouponNodeList.map((ecoCouponNodeList) => ({
-        dateValid: "Valid 1/31/24 - 02/25/24",
+        dateValid: "Valid 3/5/24 - 3/31/24",
         itemNumber:
           ecoCouponNodeList.querySelector("div.eco-items")?.textContent ?? "",
         itemName:
